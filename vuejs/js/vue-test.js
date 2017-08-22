eead1e3314279c9beba38ccc23692d71fe6afb8e
sparklv <Sparklv@outlook.com>
2017-08-22 21:38:02 +0800

    20170822

--- vuejs/js/vue-test.js
+++ vuejs/js/vue-test.js
@@ -2,12 +2,25 @@ var vm = new Vue({
 	el:"#app",
 	data:{
 		a:"bin",
-		b:"<span>lv</span>"
+		b:"<span>lv</span>",
+		val:5,
+		p1:true,
+		p2:true,
 	},
 	filters:{
 		fil1:function(val){
 				var value = val.toString();
 				return value.toUpperCase();
+		}
+	},
+	computed:{
+		reduce:function(){
+			return this.val-3;
+		}
+	},
+	methods:{
+		reduce2:function(){
+			return this.val-3;
 		}
 	}
 })
